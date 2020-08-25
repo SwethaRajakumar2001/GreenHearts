546de1faf8c459293b3ca887e48e3ed27883a055
shruti-shrz <shrutipriyasp18@gmail.com>
2020-08-25 21:11:52 +0530

    Profile related layouts

---
+++ app/src/main/java/com/example/greenhearts/Myplants.java
@@ -0,0 +1,14 @@
+package com.example.greenhearts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Myplants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myplants);
+    }
+}
