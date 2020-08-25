0b8780171ab0056080be6bc37dd2db679bbfa694
Sai Datta <cs18b044@iittp.ac.in>
2020-08-25 08:19:56 +0530

    Added layouts for Contest room display, questionnaire, join and create

---
+++ app/src/main/java/com/example/greenhearts/ContestRooms.java
@@ -0,0 +1,14 @@
+package com.example.greenhearts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContestRooms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contest_rooms);
+    }
+}
---
+++ app/src/main/java/com/example/greenhearts/create.java
@@ -0,0 +1,14 @@
+package com.example.greenhearts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
