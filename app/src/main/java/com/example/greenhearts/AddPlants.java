0e9c9da2ad7c130f1c94813f58447e0b39935f78
shruti-shrz <shrutipriyasp18@gmail.com>
2020-08-26 23:40:04 +0530

    Sending trees

--- app/src/main/java/com/example/greenhearts/AddPlants.java
+++ app/src/main/java/com/example/greenhearts/AddPlants.java
@@ -15,10 +15,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -40,44 +46,52 @@ public class AddPlants extends AppCompatActivity {
     private ImageView img;
     private Button button;
     private ListView gallery;
+    private int count =-1;
     public static final int RC_PHOTO_PICKER =2;
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 20;
-    private String current_User_Id;
-    private String current_username;
+    String url;
+   private String current_User_Id;
+   DatabaseReference cnodes;
+    FirebaseUser firebaseUser;
     private FirebaseStorage storage;
     private StorageReference storageReference;
     private Uri filepath;
     DatabaseReference db;
+    android.text.format.DateFormat df;
     FirebaseAuth mAuth= FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        df = new android.text.format.DateFormat();
+        url = "";
         setContentView(R.layout.activity_add_plants);
-        thedate = (TextView)findViewById(R.id.date);
-        btngocalendar = (ImageView)findViewById(R.id.cal);
+     //   thedate = (TextView)findViewById(R.id.date);
+    //    btngocalendar = (ImageView)findViewById(R.id.cal);
         ename = (EditText)findViewById(R.id.etreeename);
         img = (ImageView)findViewById(R.id.addtree);
         button = (Button)findViewById(R.id.addbut);
         gallery = (ListView)findViewById(R.id.gallery);
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
-        current_User_Id = mAuth.getCurrentUser().getUid();
-        current_username ="random";
+       current_User_Id = mAuth.getCurrentUser().getUid();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        cnodes = FirebaseDatabase.getInstance().getReference().child("user");
+       // current_username ="random";
         db = FirebaseDatabase.getInstance().getReference();
 //        final List<Plants> plantList = new ArrayList<>();
 //        madapter = new PlantAdapter(AddPlants.this,R.layout.mytrees_style,plantList);
 //        gallery.setAdapter(madapter);
-        Intent incoming = getIntent();
-        String date = incoming.getStringExtra("date");
-        thedate.setText(date);
-        btngocalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(AddPlants.this, CalenderActivity.class);
-                startActivity(i);
-            }
-        });
+//        Intent incoming = getIntent();
+//        String date = incoming.getStringExtra("date");
+//        thedate.setText(date);
+//        btngocalendar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(AddPlants.this, CalenderActivity.class);
+//                startActivity(i);
+//            }
+//        });
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,38 +102,56 @@ public class AddPlants extends AppCompatActivity {
             }
         });
         ename.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+        cnodes.child(current_User_Id).child("plant").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    count = (int)snapshot.getChildrenCount();
+                }else
+                {
+                    count =-1;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
               //  Plants p = new Plants(thedate.toString(),ename.getText().toString(),null);
-                final ProgressDialog progressDialog = new ProgressDialog(AddPlants.this);
-                progressDialog.setTitle("Uploading.....");
-                progressDialog.show();
-                if(filepath!=null)
+
+                if(url.isEmpty())
                 {
-                    StorageReference s = storageReference.child("Trees/"+ UUID.randomUUID().toString());
-                    s.putFile(filepath)
-                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                    progressDialog.dismiss();
-                                    Toast.makeText(AddPlants.this,"Image Uploaded",Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
-                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
-                                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
-                                }
-                            });
+                    Toast.makeText(AddPlants.this, "Empty image!!", Toast.LENGTH_SHORT).show();
+                }else
+                    if(ename.getText().toString().isEmpty())
+                    {
+                        Toast.makeText(AddPlants.this, "Empty Name!!", Toast.LENGTH_SHORT).show();
+                    }
+                else
+                {
+                    count++;
                     HashMap<String, Object> map= new HashMap<>();
-                    map.put("username",current_username );
-                    map.put("nplant",1);
-                    String tempkey = db.child("user").push().getKey();
-                    db.child("post").child(tempkey).updateChildren(map);
+                    Toast.makeText(AddPlants.this,ename.getText().toString(),Toast.LENGTH_LONG).show();
+                    map.put("plantname",ename.getText().toString());
+                    map.put("timestamp", df.format("dd MMM yyyy", new java.util.Date()));
+                    map.put("image",url);
+                    map.put("user_id",current_User_Id);
+                    String tempkey = db.child("plant").push().getKey();
+                    db.child("plant").child(tempkey).updateChildren(map);
+
+                    HashMap<String, Object> map2= new HashMap<>();
+                    map2.put("user_name",firebaseUser.getDisplayName());
+                    map2.put("no_plant",count);
+                    db.child("user").child(current_User_Id).updateChildren(map2);
                     db.child("user").child(current_User_Id).child("plant").child(tempkey).setValue(0);
+                    AddPlants.this.finish();
                 }
+
             }
         });
 }
@@ -136,6 +168,40 @@ public class AddPlants extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            upload();
+
+        }
+    }
+    private void upload()
+    {
+        final ProgressDialog progressDialog = new ProgressDialog(AddPlants.this);
+        progressDialog.setTitle("Uploading.....");
+        progressDialog.show();
+        if(filepath!=null)
+        {
+            StorageReference s = storageReference.child("Trees/"+ UUID.randomUUID().toString());
+            s.putFile(filepath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(AddPlants.this,"Image Uploaded",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    })
+                    .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            url = task.getResult().toString();
+                        }
+                    });
+
 
         }
     }
