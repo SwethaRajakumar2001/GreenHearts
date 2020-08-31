053c068df664dc3b6f733861c23d4ede4cc5a49e
Sai Datta <cs18b044@iittp.ac.in>
2020-08-31 09:10:36 +0530

    ContestRoomActivities with normal Recycler View - also needs debugging

--- app/src/main/java/com/example/greenhearts/RoomDetailsAdapter.java
+++ app/src/main/java/com/example/greenhearts/RoomDetailsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.greenhearts;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,35 +14,60 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
-public class RoomDetailsAdapter extends FirebaseRecyclerAdapter<RoomDetails, RoomDetailsAdapter.ViewHolder> {
-    
-    public RoomDetailsAdapter(@NonNull FirebaseRecyclerOptions<RoomDetails> options) {
-        super(options);
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class RoomDetailsAdapter extends RecyclerView.Adapter<RoomDetailsAdapter.ViewHolder> {
+
+    ArrayList<RoomDetails> details;
+    ArrayList<String> ids;
+    OnRoomClicked activity;
+    public RoomDetailsAdapter(Context context, ArrayList<RoomDetails> details, ArrayList<String> contest_ids) {
+        this.details=details;
+        this.ids=contest_ids;
     }
 
-    @Override
-    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull RoomDetails model) {
-        holder.tvContestName.setText(model.name);
-        if(model.imageurl!="null") {
-            Glide.with(holder.ivContestIcon.getContext()).load(model.imageurl).into(holder.ivContestIcon);
+    public interface  OnRoomClicked{
+        public void RoomClicked(int index);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView iv;
+        TextView tv;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iv=itemView.findViewById(R.id.ivContestIcon);
+            tv=itemView.findViewById(R.id.tvContest);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    activity.RoomClicked(details.indexOf(view.getTag()));
+                }
+            });
         }
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.contest_room_row, parent,false);
-        return new ViewHolder(view);
+    public RoomDetailsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v=LayoutInflater.from(parent.getContext()).inflate(R.layout.contest_room_row, parent,false);
+        return  new ViewHolder(v);
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
-        ImageView ivContestIcon;
-        TextView tvContestName;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            ivContestIcon=(ImageView)itemView.findViewById(R.id.ivContestIcon);
-            tvContestName=(TextView)itemView.findViewById(R.id.tvContest);
-
+    @Override
+    public void onBindViewHolder(@NonNull RoomDetailsAdapter.ViewHolder holder, int position) {
+        holder.itemView.setTag(details.get(position));
+        if(!(details.get(position).getImageurl().isEmpty())){
+            Glide.with(holder.iv.getContext()).load(details.get(position).getImageurl()).into(holder.iv);
         }
+        holder.tv.setText(details.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return details.size();
     }
 }
