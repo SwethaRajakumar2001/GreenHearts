609182b9970c3c9cbe1c2e6cb0c953c4e6f05e58
Sai Datta <cs18b044@iittp.ac.in>
2020-08-31 09:13:26 +0530

    the questionnaire activity

---
+++ app/src/main/java/com/example/greenhearts/QuestionnaireActivity.java
@@ -0,0 +1,14 @@
+package com.example.greenhearts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionnaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
