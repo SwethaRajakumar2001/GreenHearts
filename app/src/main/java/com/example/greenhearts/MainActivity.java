b389bb76521c52fd469832d90de82260680d4986
shruti-shrz <shrutipriyasp18@gmail.com>
2020-09-05 18:52:49 +0530

    the change

--- app/src/main/java/com/example/greenhearts/MainActivity.java
+++ app/src/main/java/com/example/greenhearts/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -62,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_second);
         mfirebasedatabse = FirebaseDatabase.getInstance();
-
         init();
         btnContest=findViewById(R.id.btnContest);
         btnProfile=findViewById(R.id.btnProfile);
