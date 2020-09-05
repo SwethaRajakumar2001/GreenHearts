9fd1cc4a4c7b68f385555e09028b6dc461ea7ddf
SwethaRajakumar2001 <cs18b037@iittp.ac.in>
2020-09-05 18:46:07 +0530

    The app name?

--- app/src/main/java/com/example/greenhearts/MainActivity.java
+++ app/src/main/java/com/example/greenhearts/MainActivity.java
@@ -61,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_second);
+
+        getSupportActionBar().setTitle("Home");
+
+
         mfirebasedatabse = FirebaseDatabase.getInstance();
 
         init();
